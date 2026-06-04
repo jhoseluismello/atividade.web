8d4cf08e15058ea19146498b1c67aa918752350e
jhoseluismello <jhoseluismello@gmail.com>
2026-06-03 21:21:48 -0300

    Mensagem do commit

---
+++ idade.js
@@ -0,0 +1,67 @@
+/*let idade = 25;
+const pais = "brasil";
+
+// Mudando o valor
+idade = 20;
+
+console.log(idade); //Exibe 20
+console.log(pais); // Exibe Brasil;
+
+// Exemplo de uso no JavaScript
+
+let preco = 50;
+let quantidade = 3;
+let frete = 15;
+
+// Calculo matematico
+let total = (preco * quantidade) + frete;
+
+console.log(total); // Resultado: 165
+
+// O Dobro
+
+let idade = 50;
+const pais = "brasil";
+
+idade = 25;
+
+// A Troca
+
+let a = 10;
+console.log("a: + a");
+let b = 20
+console.log ("b: "+b);
+let c = a;
+a = b;
+b = a;
+console.log("A trocs de a e b é")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Variaveis*/
+
+/*let imc = 18.5;
+if (imc <=18.5) {
+    console.log("Abaixo do peso")
+
+} else if (imc >=18.6 && imc <=24.9){
+    console.log("Peso normal")
+
+} else if (imc >=25 && imc <=29.9){
+    console.log ("Sobrepeso")
+
+}else {
+    console.log ("Obesidade")
+}*/
+
+
+
+let numero;
+numero =10
+
+if(numero % 2 ==0) {
+    console.log(" O número é par");
+}else{
+    console.log("O número é ímpar")
+}
