9d3f5eff079f25633dc393df586273f8b06e4e91
jhoseluismello <jhoseluismello@gmail.com>
2026-06-10 21:31:55 -0300

    Revisão UC2

---
+++ revisão.js
@@ -0,0 +1,33 @@
+// AlGORITMO: calcular o média de 3 notas
+// Passo 1 - receber as notas
+
+/*const nota1 = 7.5;
+const nota2 = 8.0;
+const nota3 = 6.5;
+
+// Passo 2 - somar as notas
+const soma = nota1 + nota2 + nota3;
+
+// Passo 3 - dividir pelo quantidade de notas
+const quantidade = 3;
+const media = soma / quantidade;
+
+// Passo 4 - exibir o resultado
+console.log("Média:", media);0*/ // Saída: Média 7.333...
+
+
+// const: valor fixo após a declaração
+const PI = 3.14159;
+const nomeAluno = "Maria";
+
+// PI = 3.14; // ERRO! Não pode reatribuir uma constante
+
+// let: pode mudar
+let contador = 0;
+contador = contador + 1; // Ok - agora contador vale 1
+
+// var: forma antiga (evite em códito novo)
+var idade = 20;
+idade = 21; // funciona, mas prefira let/const
+
+console.log ({PI, nomeAluno, contador, idade});
